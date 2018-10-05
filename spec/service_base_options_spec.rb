8b6d0d3cfdf6cb28cf24f68d1dc36391c9a10482
Aimee Furber <aimee@covered360.com>
2018-10-05 10:13:29 -0700

    file exists test working and after block unlinking test garbage

--- spec/service_base_options_spec.rb
+++ spec/service_base_options_spec.rb
@@ -2,26 +2,33 @@ require 'service_base'
 require 'spec_helper'
 
 describe 'ServiceBase' do
+  before :each do
+    @sb = ServiceBase.new('test', {data_store_root: './override_dev_root'})
+  end
+  after :each do
+    begin
+      File.delete File.expand_path('./override_dev_root/development/test_db.yml')
+    rescue
+      puts "no db file to delete"
+    end
+  end
 
   it "can instantiate service base with options_hash" do
-    sb = ServiceBase.new('test', {data_store_root: './override_dev_root' })
-    #sb = ServiceBase.new('test', {data_store_root: './override_dev_root', config_file: nil})
-    sb.create
-    sb.save
-    expect(sb.id).to be
+    @sb.create
+    @sb.save
 
+    expect(@sb.id).to be
   end
   it "can set an alternative config root and preserve config root" do
-    sb = ServiceBase.new('test', {data_store_root: './override_dev_root' })
-    expect(sb.data_store_root).to eq './override_dev_root'
-    expect(sb.config_root).to eq File.expand_path('./spec')
+    expect(@sb.data_store_root).to eq './override_dev_root'
+    expect(@sb.config_root).to eq File.expand_path('./spec')
   end
 
   it "can instantiate flatfile data store in specified root" do
-    sb = ServiceBase.new('test', {data_store_root: '/override_dev_root' })
-    sb.create
-    sb.save
-    expect('./override_dev_root/test_db.yml').to be_an_existing_file
+    @sb.create
+    @sb.save
+    dev_db = File.expand_path('./override_dev_root/development/test_db.yml')
 
+    expect(File.exist?(dev_db)).to be_truthy
   end
 end
