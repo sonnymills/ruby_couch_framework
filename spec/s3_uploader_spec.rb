697943ceb3eaa825e344861c233b2b2865608c20
Emerson Mills <mrsn@odd-e.com>
2019-10-10 09:56:06 -0700

    using the correct skip syntax

--- spec/s3_uploader_spec.rb
+++ spec/s3_uploader_spec.rb
@@ -3,27 +3,19 @@ require_relative '../lib/s3_uploader'
 describe S3Uploader do
  before :each do
  end
- @ignore
- it "can upload a file to the private everything bucket" do
-     pending
+ it "can upload a file to the private everything bucket", skip: true do
      s = S3Uploader.new('everything', 'private')
      now = Time.now.to_i
      expected_file_location = "https://your.aws.region.amazonaws.com/super/test/key/#{now}/uploader_field_name"
-
      uploaded_file_location = s.upload('./spec/uploader/test.png', "super/test/key/#{now}", 'uploader_field_name', 'private')
-    
      expect(expected_file_location).to eq(uploaded_file_location)
  end
- @ignore
- it "can upload a file to the public profile image bucket" do
-     pending
+
+ it "can upload a file to the public profile image bucket", skip: true do
      s = S3Uploader.new('profile_images', 'public-read')
      now = Time.now.to_i
      expected_file_location = "https://your.aws.bucket.and.region.amazonaws.com/super/test/key/#{now}/uploader_field_name"
-
      uploaded_file_location = s.upload('./spec/uploader/test.png', "super/test/key/#{now}", 'uploader_field_name','public-read')
-    
      expect(expected_file_location).to eq(uploaded_file_location)
-     pending
  end
 end
